3bc866bbb0745a60b995bce3bf70ae688cd5b636
Tim Koschützki <tim@debuggable.com>
2009-07-06 10:32:08 +0200

    appeal and attachment relations

--- app/models/appeal.php
+++ app/models/appeal.php
@@ -1,5 +1,12 @@
 <?php
 class Appeal extends AppModel {
-	
+	var $belongsTo = array(
+		'User',
+		'Country',
+		'Parent' => array(
+			'className' => 'Appeal',
+			'foreignKey' => 'parent_id'
+		)
+	);
 }
 ?>
--- app/models/attachment.php
+++ app/models/attachment.php
@@ -1,5 +1,12 @@
 <?php
 class Attachment extends AppModel {
-	
+	var $belongsTo = array(
+		'Comment' => array(
+			'foreignKey' => 'foreign_id'
+		),
+		'Post' => array(
+			'foreignKey' => 'foreign_id'
+		)
+	);
 }
 ?>
