b1726703f82f93ecd9268a052e9a9ff90703fcd8
Tim Koschützki <tim@debuggable.com>
2009-08-10 14:29:18 +0200

    refactoring caplimit component

--- app/controllers/components/caplimit.php
+++ app/controllers/components/caplimit.php
@@ -13,104 +13,108 @@
  * @author		 mayank.bhramar@enova-tech.net
  *
  */
-
 class CaplimitComponent extends Object {
-	var $name= 'Caplimit'; 
+	var $name = 'Caplimit';
 	var $tellFriend;
-	
-	
+/**
+ * undocumented function
+ *
+ * @param string $controller 
+ * @return void
+ * @access public
+ */
 	function initialize(&$controller) {
-			// saving the controller reference for later use
-			$this->controller =& $controller;
-			App::import('Model','TellFriend');
-		    $this->tellFriend = new TellFriend();
+		$this->controller =& $controller;
+		$this->tellFriend = ClassRegistry::init('TellFriend');
 	}
-	
+/**
+ * undocumented function
+ *
+ * @param string $emails 
+ * @return void
+ * @access public
+ */
 	function checkCaps($emails) {
-		if($this->checkForIpSpam() == false) {
-		  Configure::write('App.tellafriendError','Sorry. You have been banned from sending more emails. Please try after sometime.');
-		  return false;
-     	}
-		if($this->checkForSpamEmail($emails)!='passed') {
-		  $faultyEmail = $this->checkForSpamEmail($emails);
-		  Configure::write('App.tellafriendError','Sorry. The email '.$faultyEmail.' has already been used many times. Please send again to another email.');
-		  return false;
+		if ($this->checkForIpSpam() == false) {
+			$msg = 'Sorry. You have been banned from sending more emails.';
+			$msg .= ' Please try after sometime.';
+			Configure::write('App.tellafriendError', $msg);
+			return false;
 		}
-		 
-		   
-		return true;   
-		//pr($emails);
+
+		if ($this->checkForSpamEmail($emails) != 'passed') {
+			$faultyEmail = $this->checkForSpamEmail($emails);
+			$msg = 'Sorry. The email ' . $faultyEmail . ' has already been used many times.';
+			$msg .= ' Please send again to another email.';
+			Configure::write('App.tellafriendError', $msg);
+			return false;
+		}
+		return true;
 	}
-	
-	
+/**
+ * undocumented function
+ *
+ * @param string $emails 
+ * @return void
+ * @access public
+ */
 	function checkForSpamEmail($emails) {
-	  $toCheck = $this->tellFriend->getEmailsSentInTime();
-	  foreach($emails as $email) {
-	    foreach($toCheck as $emailToCheck => $counts) {
-		  if($email == $emailToCheck) {
-		    if($counts > Configure::read('App.emailsPerDay') ) {
-			  return $emailToCheck;
+		$toCheck = $this->tellFriend->getEmailsSentInTime();
+
+		foreach ($emails as $email) {
+			foreach ($toCheck as $emailToCheck => $counts) {
+				if ($email == $emailToCheck) {
+					if ($counts > Configure::read('App.emailsPerDay') ) {
+						return $emailToCheck;
+					}
+				}
 			}
-		  }
 		}
-	  }
-	  return 'passed';
-	
+		return 'passed';
 	}
-	
+/**
+ * undocumented function
+ *
+ * @return void
+ * @access public
+ */
 	function checkForIpSpam() {
-	 $currentIP = $this->getIP();
-	 $isIpValid = $this->tellFriend->isIpSpamming($currentIP);
-		 if($isIpValid != 0) {
-		   return false;	 
-		 } 
-		 else
-	       return true;	 
+		$ip = RequestHandlerComponent::getClientIP;
+		return $this->tellFriend->isIpSpamming($ip) == 0;
 	}
-	
-	
-	function getIP() {
-	 return RequestHandlerComponent::getClientIP();	
-	}
-	
-	
-	
-	
-	function getTimeDifference( $start, $end )
-	{
-		$uts['start']      =    strtotime( $start );
-		$uts['end']        =    strtotime( $end );
-		if( $uts['start']!==-1 && $uts['end']!==-1 )
-		{
-			if( $uts['end'] >= $uts['start'] )
-			{
-				$diff    =    $uts['end'] - $uts['start'];
-				if( $days=intval((floor($diff/86400))) )
+/**
+ * undocumented function
+ *
+ * @param string $start 
+ * @param string $end 
+ * @return void
+ * @access public
+ */
+	function getTimeDifference($start, $end) {
+		$uts['start'] = strtotime( $start );
+		$uts['end'] = strtotime( $end );
+
+		if ($uts['start'] !== -1 && $uts['end'] !== -1) {
+			if ($uts['end'] >= $uts['start']) {
+				$diff = $uts['end'] - $uts['start'];
+
+				if ($days= intval(floor($diff / 86400))) {
 					$diff = $diff % 86400;
-				if( $hours=intval((floor($diff/3600))) )
+				}
+				if ($hours = intval(floor($diff / 3600))) {
 					$diff = $diff % 3600;
-				if( $minutes=intval((floor($diff/60))) )
+				}
+				if ($minutes = intval(floor($diff / 60)) ) {
 					$diff = $diff % 60;
-				$diff    =    intval( $diff );            
-				return( array('days'=>$days, 'hours'=>$hours, 'minutes'=>$minutes, 'seconds'=>$diff) );
+				}
+
+				$seconds = intval($diff);            
+				return compact('days', 'hours', 'minutes', 'seconds');
 			}
-			else
-			{
-				trigger_error( "Ending date/time is earlier than the start date/time", E_USER_WARNING );
-			}
+			trigger_error("Ending date/time is earlier than the start date/time", E_USER_WARNING);
 		}
-		else
-		{
-			trigger_error( "Invalid date/time data detected", E_USER_WARNING );
-		}
-		return( false );
+		trigger_error( "Invalid date/time data detected", E_USER_WARNING );
+		return false;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
 }
+?>
