536a617691d35642f61a111a90abc55d9263f96f
Tim Koschützki <tim@debuggable.com>
2009-08-11 13:52:23 +0200

    other amount checkbox is checked if input box gets focus

--- app/webroot/js/views/gifts/add.js
+++ app/webroot/js/views/gifts/add.js
@@ -1,8 +1,15 @@
 $(function() {
 	if ($('#txtOtherAmount').val() != '') {
+		checkOtherAmount();
+	};
+	$('#txtOtherAmount').focus(function() {
+		checkOtherAmount();
+	})
+
+	function checkOtherAmount() {
 		$('input.otheramount').attr('checked', true);
 		$('input.amount').each(function() {
 			$(this).attr('checked', false);
 		});
-	};
+	}
 });
