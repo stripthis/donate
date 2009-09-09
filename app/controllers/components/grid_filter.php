e32936af519ba90f73d92f33ed9d9568138f34d9
Tim Koschützki <tim@debuggable.com>
2009-09-09 21:29:22 +0200

    autoselect january in daterange filter if no months chosen

--- app/controllers/components/grid_filter.php
+++ app/controllers/components/grid_filter.php
@@ -10,13 +10,19 @@ class GridFilterComponent extends Object {
  * @return void
  * @access public
  */
-	function dateRange($condtitions, $params, $model, $field) {
-		if (!empty($params['start_date_year']) && !empty($params['start_date_month'])) {
+	function dateRange($condtitions, &$params, $model, $field) {
+		if (!empty($params['start_date_year'])) {
+			if (empty($params['start_date_month'])) {
+				$params['start_date_month'] = '01';
+			}
 			$startDate = '01-' . $params['start_date_month'] . '-' . $params['start_date_year'];
 			$condtitions[] = "DATE_FORMAT(" . $model . "." . $field. ", '%Y-%m-%d') >= '" . $startDate . "'";
 		}
 
-		if (!empty($params['end_date_year']) && !empty($params['end_date_month'])) {
+		if (!empty($params['end_date_year'])) {
+			if (empty($params['end_date_month'])) {
+				$params['end_date_month'] = '01';
+			}
 			$endDate = '01-' . $params['end_date_month'] . '-' . $params['end_date_year'];
 			$condtitions[] = "DATE_FORMAT(" . $model . "." . $field. ", '%Y-%m-%d') <= '" . $endDate . "'";
 		}
