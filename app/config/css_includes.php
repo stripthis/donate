f23e0faba7fceeefd7f5a36f1353285761c34263
Tim Koschüzki <tim@debuggable.com>
2009-10-01 13:09:38 +0200

    css loading updates

--- app/config/css_includes.php
+++ app/config/css_includes.php
@@ -1,17 +1,10 @@
 <?php
 $config = array(
 	'CssIncludes' => array(
-    'default.css' => '*:*,!pages:admin_*',
-    'admin.css' => "*:admin_*",
-    'tooltip.css' => "*:*",
-    //'jquery.checkbox.css' => "*:admin_*",
-    //'ceo.css' => '*:*',
-    //'leaders.css' => '*:*',
-    //'login.css' => 'auth:login',
-    //'register.css' => 'users:register',
-    //'dashboard.css' => '*:dashboard',
-    //'posts.css' => 'posts:index'
-		// 'login.css' => '*:*',
+		'admin.css' => '*:admin_*',
+		'tooltip.css' => '*:*',
+		'generic.css' => '*:*,!pages:admin_*',
+
 	)
 );
 ?>
