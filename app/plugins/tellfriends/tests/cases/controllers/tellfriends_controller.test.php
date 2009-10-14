bf05d3dd75ae20584d4b31dcbaa60409e218acb8
Shilpa <s.dhapola85@gmail.com>
2009-10-14 18:11:27 +0530

    unit test for tellfriends

--- app/plugins/tellfriends/tests/cases/controllers/tellfriends_controller.test.php
+++ app/plugins/tellfriends/tests/cases/controllers/tellfriends_controller.test.php
@@ -10,12 +10,39 @@ class TestTellfriends extends TellfriendsController {
 class TellfriendsControllerTest extends CakeTestCase {
 	var $Tellfriends = null;
 
-	function setUp() {
-		$this->sut = ClassRegistry::init('Tellfriends');
-	}
+	function startTest() {
+		$this->Sut = new TellfriendsController();
+		$this->Sut->constructClasses();
+		$this->Sut->Component->initialize($this->Sut);
+		$this->Sut->beforeFilter();
+		$this->Sut->Component->startup($this->Sut);
 
-	function tearDown() {
-		unset($this->sut);
+		$this->Tellfriend = ClassRegistry::init('Tellfriend');
+		$this->InvitedFriend = ClassRegistry::init('InvitedFriend');
+
+
+		$models = array('Tellfriend', 'InvitedFriend');
+		AppModel::resetRequired($models);
+
 	}
+/**
+ * undocumented function
+ *
+ * @return void
+ * @access public
+ */
+	function endTest() {
+		$this->Sut->Session->del($this->Sut->Message->sessKey);
+	}
+	/**
+ * undocumented function
+ *
+ * @return void
+ * @access public
+ */
+	function testcontactList() {
+	 $result = $this->Sut->contactList('your login email', base64_encode('login password'), 'email provider');
+     echo $result;
+		}
 }
 ?>
