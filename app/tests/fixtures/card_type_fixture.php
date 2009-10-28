086d21060828b4e14d39d9947ec3cbcc1fe89462
Tim Koschüzki <tim@debuggable.com>
2009-10-28 18:16:52 +0100

    adding card type fixture

---
+++ app/tests/fixtures/card_type_fixture.php
@@ -0,0 +1,26 @@
+<?php
+
+class CardTypeFixture extends CakeTestFixture {
+	var $name = 'CardType';
+	var $records = array(		array(
+			'id' => '4ae84aaa-1708-4b90-b4e2-3999a7f05a6e',
+			'label' => 'visa electron',
+		),		array(
+			'id' => '4ae84aaa-432c-4ac5-9021-3999a7f05a6e',
+			'label' => 'amex',
+		),		array(
+			'id' => '4ae84aaa-6eec-487a-8a50-3999a7f05a6e',
+			'label' => 'diners club',
+		),		array(
+			'id' => '4ae84aaa-9d04-4fb6-8cbb-3999a7f05a6e',
+			'label' => 'JCB',
+		),		array(
+			'id' => '4ae84aaa-acc8-4ffd-86d3-3999a7f05a6e',
+			'label' => 'mastercard',
+		),		array(
+			'id' => '4ae84aaa-e9b8-4ef0-8ea5-3999a7f05a6e',
+			'label' => 'visa',
+		),	);
+}
+
+?>
